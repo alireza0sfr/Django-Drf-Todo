4fec96252ac766ec7d7807c38bfb2c003fe9eff3
Alireza Safari <alireza.safaree@gmail.com>
2023-06-03 22:37:04 +0330

    feat(task): changes user to profile in task model

--- task/models.py
+++ task/models.py
@@ -1,14 +1,11 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 from common.models import BaseModel
 from enums.base import TaskStatus
 
-User = get_user_model()
-
 
 class Task(BaseModel):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
     status = models.BooleanField(default=TaskStatus.PUBLISHED)
