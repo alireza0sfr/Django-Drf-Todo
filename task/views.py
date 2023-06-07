35136f057ccda3652f62860ce73596f9cbfd64af
Alireza Safari <alireza.safaree@gmail.com>
2023-06-07 20:07:58 +0330

    refactor(task): removes get_queryset from TaskModelViewSet

--- task/views.py
+++ task/views.py
@@ -2,23 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from serializers.task.serializers import TaskModelSerializer, CategoryModelSerializer
 from task.models import Task, Category
-from enums.task import TaskStatus
 
 
 class TaskModelViewSet(ModelViewSet):
     permission_classes = []
     serializer_class = TaskModelSerializer
-    queryset = Task.objects.filter(status=TaskStatus.PUBLISHED)
-    # model = Task
-
-    # def get_queryset(self):
-    #     if self.action == 'list':
-    #         return self.model.objects.filter(status=TaskStatus.PUBLISHED)
-    #     elif self.action == 'retrieve':
-    #         return self.model.objects.filter(id=self.kwargs['pk'])
-    #     else:
-    #         return super().get_queryset()
-
+    queryset = Task.objects.all()
 
 
 class CategoryViewSet(ModelViewSet):
