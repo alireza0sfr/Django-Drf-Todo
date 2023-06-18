2bcda984f039ed97fd6bd69d7c458f81e46d8ce0
Alireza Safari <alireza.safaree@gmail.com>
2023-06-18 20:33:23 +0330

    feat(common): creates Custom BasePermission

--- common/permissions.py
+++ common/permissions.py
@@ -1,4 +1,14 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
+
+
+class BasePermission(permissions.BasePermission):
+    """ BasePermission class adds all permissions for superUsers """
+
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user and request.user.is_superuser)
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
 
 
 class IsSuperUser(BasePermission):
