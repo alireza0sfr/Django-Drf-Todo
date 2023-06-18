181b40721a98842136e84d927d8e8ab26e847d8d
Alireza Safari <alireza.safaree@gmail.com>
2023-06-18 21:37:44 +0330

    feat(serializers): set author of task based on request user

--- serializers/task/serializers.py
+++ serializers/task/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer, ReadOnlyField
 
 from task.models import Task, Category
+from accounts.models import Profile
 
 
 class TaskModelSerializer(ModelSerializer):
@@ -9,6 +10,11 @@ class TaskModelSerializer(ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'author', 'title', 'content', 'snippet', 'status', 'category', 'priority', 'created_date']
+        read_only_fields = ['author']
+
+    def create(self, validated_data):
+        validated_data['author'] = Profile.objects.get(user__id=self.context.get('request').user.id)
+        return super().create(validated_data)
 
 
 class CategoryModelSerializer(ModelSerializer):
