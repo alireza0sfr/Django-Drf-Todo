44e5961a9e5cda29c611610484fa00b377825363
Alireza Safari <alireza.safaree@gmail.com>
2023-06-18 21:37:25 +0330

    feat(task): adds permission_classes to viewSets

--- common/permissions.py
+++ common/permissions.py
@@ -5,14 +5,22 @@ class BasePermission(permissions.BasePermission):
     """ BasePermission class adds all permissions for superUsers """
 
     def has_object_permission(self, request, view, obj):
-        return bool(request.user and request.user.is_superuser)
+        return request.method in permissions.SAFE_METHODS and bool(request.user and request.user.is_superuser)
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_superuser)
+        return request.method in permissions.SAFE_METHODS and bool(request.user and request.user.is_superuser)
 
 
 class IsSuperUser(BasePermission):
     """Allows access only to superusers."""
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_superuser)
+        parent_access = super().has_permission(request, view)
+        return parent_access or bool(request.user and request.user.is_superuser)
+
+
+class isOwnerOrReadonly(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        parent_access = super().has_object_permission(request, view, obj)
+        return parent_access or obj.author == request.user.id
--- task/views.py
+++ task/views.py
@@ -2,15 +2,15 @@ from rest_framework.viewsets import ModelViewSet
 
 from serializers.task.serializers import TaskModelSerializer, CategoryModelSerializer
 from task.models import Task, Category
-
+from common.permissions import isOwnerOrReadonly
 
 class TaskModelViewSet(ModelViewSet):
-    permission_classes = []
+    permission_classes = [isOwnerOrReadonly]
     serializer_class = TaskModelSerializer
     queryset = Task.objects.all()
 
 
 class CategoryViewSet(ModelViewSet):
-    permission_classes = []
+    permission_classes = [isOwnerOrReadonly]
     serializer_class = CategoryModelSerializer
     queryset = Category.objects.all()
