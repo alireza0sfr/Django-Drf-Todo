b848b757b876e5a262712549871ea41729b3e561
Alireza Safari <alireza.safaree@gmail.com>
2023-06-24 00:24:45 +0330

    fix(api): fixes v1 urls to include accounts urls with app basename

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -1,3 +1,5 @@
+from django.urls import include, path
+
 from api.router import router
 from task.views import TaskModelViewSet, CategoryViewSet
 from accounts.urls import urlpatterns as accounts_urlpatterns
@@ -5,5 +7,7 @@ from accounts.urls import urlpatterns as accounts_urlpatterns
 router.register('tasks', TaskModelViewSet, basename='tasks')
 router.register('categories', CategoryViewSet, basename='categories')
 
-urlpatterns = router.urls
-urlpatterns += accounts_urlpatterns
+urlpatterns = [
+    path('', include(router.urls)),
+    path('accounts/', include(accounts_urlpatterns)),
+]
