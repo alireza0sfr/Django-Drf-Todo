e9f59b8ab6c81c02028bf6c040e20fd872183f60
Alireza Safari <alireza.safaree@gmail.com>
2023-07-02 22:30:08 +0330

    fix(settings): fixes cors allowed origins

--- core/core/settings.py
+++ core/core/settings.py
@@ -194,7 +194,7 @@ PHONENUMBER_DEFAULT_FORMAT = 'E164'
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080/',
-    'http://localhost:8081/'
+    'http://localhost:8080',
+    'http://localhost:8081'
 ]
 CORS_ALLOW_ALL_ORIGINS = False
